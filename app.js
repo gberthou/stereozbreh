29c78fd1edfcc8669b2be59bbf830401b3e0d5e3
Gautier Berthou <gautier.berthou@outlook.fr>
2020-01-18 16:36:57 +0100

    Minor refactoring

--- app.js
+++ app.js
@@ -17,25 +17,53 @@ var fragmentShaderText =
 "}"
 ].join("\n");
 
-var InitDemo = function ()
+
+var cubeIndices =
+[
+    0, 1, 2,
+    0, 2, 3,
+
+    4, 5, 6,
+    4, 6, 7,
+
+    4, 0, 3,
+    4, 3, 7,
+
+    1, 5, 6,
+    1, 6, 2,
+
+    0, 1, 5,
+    0, 5, 4,
+
+    3, 2, 6,
+    3, 6, 7
+];
+
+function initGL()
 {
 	var canvas = document.getElementById("game-surface");
-	var gl = canvas.getContext("webgl");
+	var _gl = canvas.getContext("webgl");
 
-	if (!gl)
+	if (!_gl)
     {
 		console.log("WebGL not supported, falling back on experimental-webgl");
-		gl = canvas.getContext("experimental-webgl");
-        if (!gl)
+		_gl = canvas.getContext("experimental-webgl");
+        if (!_gl)
         {
             alert("Your browser does not support WebGL");
             return;
         }
-	}
+    }
 
-	//
-	// Create shaders
-	// 
+    return {
+        gl: _gl,
+        width: parseInt(canvas.getAttribute("width"), 10),
+        height: parseInt(canvas.getAttribute("height"), 10)
+    };
+}
+
+function initShaders(gl)
+{
 	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
 	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
@@ -56,27 +84,32 @@ var InitDemo = function ()
 		return;
 	}
 
-	var program = gl.createProgram();
-	gl.attachShader(program, vertexShader);
-	gl.attachShader(program, fragmentShader);
-	gl.linkProgram(program);
-	if (!gl.getProgramParameter(program, gl.LINK_STATUS))
+	var _program = gl.createProgram();
+	gl.attachShader(_program, vertexShader);
+	gl.attachShader(_program, fragmentShader);
+	gl.linkProgram(_program);
+	if (!gl.getProgramParameter(_program, gl.LINK_STATUS))
     {
-		console.error("ERROR linking program!", gl.getProgramInfoLog(program));
+		console.error("ERROR linking program!", gl.getProgramInfoLog(_program));
 		return;
 	}
-	gl.validateProgram(program);
-	if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS))
+	gl.validateProgram(_program);
+	if (!gl.getProgramParameter(_program, gl.VALIDATE_STATUS))
     {
-		console.error("ERROR validating program!", gl.getProgramInfoLog(program));
+		console.error("ERROR validating program!", gl.getProgramInfoLog(_program));
 		return;
 	}
 
-    vPosition = gl.getAttribLocation(program, "position");
+    return {
+        program : _program,
+        vPosition : gl.getAttribLocation(_program, "position"),
+        vMview : gl.getUniformLocation(_program, "mview"),
+        vMproj : gl.getUniformLocation(_program, "mproj")
+    };
+}
 
-    vMview = gl.getUniformLocation(program, "mview");
-    vMproj = gl.getUniformLocation(program, "mproj");
-
+function initScene(gl, shader)
+{
     var cubeVertices = 
     [
         -1, -1, -1,
@@ -90,27 +123,6 @@ var InitDemo = function ()
         -1, 1, 1
     ];
 
-    var cubeIndices =
-    [
-        0, 1, 2,
-        0, 2, 3,
-
-        4, 5, 6,
-        4, 6, 7,
-
-        4, 0, 3,
-        4, 3, 7,
-
-        1, 5, 6,
-        1, 6, 2,
-
-        0, 1, 5,
-        0, 5, 4,
-
-        3, 2, 6,
-        3, 6, 7
-    ];
-
     var vbo = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeVertices), gl.STATIC_DRAW);
@@ -119,9 +131,8 @@ var InitDemo = function ()
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeIndices), gl.STATIC_DRAW);
     
-    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, gl.FALSE, 3*Float32Array.BYTES_PER_ELEMENT, 0);
-    gl.enableVertexAttribArray(vPosition);
-
+    gl.vertexAttribPointer(shader.vPosition, 3, gl.FLOAT, gl.FALSE, 3*Float32Array.BYTES_PER_ELEMENT, 0);
+    gl.enableVertexAttribArray(shader.vPosition);
 
     var viewMatrix = new Float32Array(16);
     glMatrix.mat4.lookAt(viewMatrix, glMatrix.vec3.fromValues(0, -2, 1), glMatrix.vec3.fromValues(0, 0, 0), glMatrix.vec3.fromValues(0, 0, 1));
@@ -130,19 +141,30 @@ var InitDemo = function ()
     glMatrix.mat4.ortho(projMatrix, -2, 2, -2, 2, .1, 2);
     //glMatrix.mat4.perspective(projMatrix, 100*Math.PI/180, 4./3, .1, 2);
 
-	gl.useProgram(program);
-    gl.uniformMatrix4fv(vMview, false, viewMatrix);
-    gl.uniformMatrix4fv(vMproj, false, projMatrix);
+	gl.useProgram(shader.program);
+    gl.uniformMatrix4fv(shader.vMview, false, viewMatrix);
+    gl.uniformMatrix4fv(shader.vMproj, false, projMatrix);
 
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
+}
 
-	//
-	// Main render loop
-	//
+function draw(gl)
+{
 	gl.clearColor(0, 0, 0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+    //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
     gl.drawElements(gl.TRIANGLES, cubeIndices.length, gl.UNSIGNED_SHORT, 0);
+}
+
+function initDemo()
+{
+    var context = initGL();
+    var gl = context.gl;
+
+    shader = initShaders(context.gl);
+    initScene(context.gl, shader);
+
+    draw(context.gl);
 };
